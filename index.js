2ae9ff0dd274b4d99256d17fd0190a431ee3e92d
Víctor Peña <victortkd88@gmail.com>
2023-06-06 20:14:25 -0600

    agregando ejercicios

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { mayor, menor, sumatoria } = require('./operaciones')
+const { mayor, menor, sumatoria, listaOrdenada } = require('./operaciones')
 const arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1, 413, 413,4, 1, 413, 4, 0, 4, 23]
 
 console.log(mayor(arrNumbers));
 console.log(menor(arrNumbers));
 console.log(sumatoria(arrNumbers));
+console.log(listaOrdenada(arrNumbers));
+
 
 
 
--- operaciones.js
+++ operaciones.js
@@ -1,37 +1,47 @@
 /**
- * Esta funcion retorna el numero mayor
- * @param {mayor} function 
+ * Esta funcion retorna el numero mayor del array
+ * agarrando el primer numero del array y comparandolo
+ * hasta encontrar el mayor
+ * @param {mayor} arr
  * @returns 
  */
-function mayor (arr) {
-  let numeroMayor = arr[0]
+function mayor(arr) {
+  let numeroMayor = arr[0];
 
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] > numeroMayor) {
-      numeroMayor = arr[i]
+  arr.forEach((num) => {
+    if (num > numeroMayor) {
+      numeroMayor = num;
     }
-  }
-  
-  return numeroMayor
+  });
+
+  return numeroMayor;
 }
 
 /**
- * Esta funcion retorna el numero menor
- * @param {menor} function 
+ * Esta funcion retorna el numero menor del array
+ * agarrando el primer numero del array y comparandolo
+ * hasta encontrar el menor
+ * @param {menor} arr 
  * @returns 
  */
-function menor (arr) {
-  let numeroMenor = arr[0]
+function menor(arr) {
+  let numeroMenor = arr[0];
 
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] < numeroMenor) {
-      numeroMenor = arr[i]
+  arr.forEach((num) => {
+    if (num < numeroMenor) {
+      numeroMenor = num;
     }
-  }
-  
-  return numeroMenor
+  });
+
+  return numeroMenor;
 }
 
+/**
+ * Esta funcion retorna la sumatoria de todos los numeros del array
+ * 
+ * @param {sumatoria} arr 
+ * @returns 
+ */
 function sumatoria(arr) {
   let suma = 0;
   for(let i = 0; i < arr.length; i++) suma += arr[i];
@@ -39,19 +49,28 @@ function sumatoria(arr) {
   return suma
 }
 
+/**
+ * Esta funcion retorna un array con todos sus elementos ordenados (Números).
+ * comparar pares de elementos adyacentes. Si el elemento actual es mayor que el siguiente, se intercambian de posición.
+ * 
+ * @param {listaOrdenada} arr 
+ * @returns 
+ */
 function listaOrdenada(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
+  let flag = false;
+
+  do {
+    flag = false;
+
+    for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        flag = true;
       }
     }
-  }
+  } while (flag);
+
+  return arr;
 }
 
-
-
-
-module.exports = { mayor, menor, sumatoria }
+module.exports = { mayor, menor, sumatoria, listaOrdenada }
